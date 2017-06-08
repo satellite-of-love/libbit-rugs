a687075fe370c151375b519acf98b7c547dbdcdb
Jessica Kerr <jessitron@gmail.com>
2017-06-08 10:05:57 -0500

    The sample works here

--- .atomist/editors/SampleLibbit.ts
+++ .atomist/editors/SampleLibbit.ts
@@ -4,26 +4,35 @@ import { EditProject } from "@atomist/rug/operations/ProjectEditor";
 import { Pattern } from "@atomist/rug/operations/RugOperation";
 
 /**
- * Sample TypeScript editor used by AddSampleLibbit.
+ * Run this to copy FEATURE into your project
  */
 @Editor("SampleLibbit", "Run this to copy FEATURE into your project")
-@Tags("documentation")
+@Tags("libbit")
 export class SampleLibbit implements EditProject {
 
-    @Parameter({
-        displayName: "Some Input",
-        description: "example of how to specify a parameter using decorators",
-        pattern: Pattern.any,
-        validInput: "a description of the valid input",
-        minLength: 1,
-        maxLength: 100,
-    })
-    public inputParameter: string;
-
     public edit(project: Project) {
-        const certainFile = project.findFile("hello.txt");
-        const newContent = certainFile.content.replace(/the world/, this.inputParameter);
-        certainFile.setContent(newContent);
+
+        const sourceDir = "/src/main/java/com/jessitron/exportme";
+        const testDir = "/src/test/java/com/jessitron/exportme";
+
+        const sourceFilenames = ["SomeCode.java"];
+        const testFilenames = ["SomeTest.java"];
+
+        const sourceFiles = sourceFilenames.map((n) =>
+            sourceDir + "/" + n);
+
+        const testFiles = testFilenames.map((n) =>
+            testDir + "/" + n);
+
+        sourceFiles.concat(testFiles).forEach((f) => {
+            if (project.fileExists(f)) {
+                console.log("File ${f} already exists. Exiting");
+                return;
+            }
+            project.copyEditorBackingFileOrFail(f);
+        });
+
+        // TODO: some atomist config operation to make a link
     }
 }
 
